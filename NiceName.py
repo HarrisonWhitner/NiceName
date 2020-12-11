a8486786c3354cb36024e2c3865cab7cbf51f50b
HarrisonWhitner <hwhit16@vt.edu>
2020-12-11 17:48:56 -0500

    Add sklearn to NiceName

--- NiceName.py
+++ NiceName.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import utilities as util
+from sklean.neighbors import KNeighborsClassifier
+
 """ NiceName
 A python package for detecting offensive usernames.
 
@@ -9,12 +12,28 @@ Authors: Harrison Whitner, Jeremy Howe
 Creation Date: 11/28/20
 """
 
+
+""" CONSTANTS """
+
 TRAINING_SET_FILENAME = 'data/generated/training_set.txt'
+
+
+""" FUNCTIONS """
+
 
 def generate_training_set(n_samples: int, percent_offensive=0.5, percent_obfuscated=0.5, debug=False):
     """Create data which the KNN used to predict usernames is fit upon."""
     pass
 
-def predict_username_is_offensive(username: str) -> bool:
+
+def predict_username_is_offensive(username: str, n_neighbors=5, weights='uniform', debug=False) -> bool:
     """Given a username, predict whether it is offensive or inoffensive using a KNN fitted on generated usernames."""
-    pass
+    
+    training_data = util.file_to_array(TRAINING_SET_FILENAME)
+    features = []
+    targets = []
+
+    for row in training_data:
+        split_row = row.split()
+        features.append([float(x) for x in split_row[1:4]])
+        targets.append(int(split_row[4]))
