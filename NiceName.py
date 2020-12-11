dfba5993f836b6f42ca45456d84e379337deaba3
jhowe <jhowe@localhost.localdomain>
2020-12-11 17:34:54 -0500

    Updated data generation to use even distributions of good and bad words. Updated NiceName to use new data generation.

--- NiceName.py
+++ NiceName.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from data_generator import generate_usernames
 
 """ NiceName
 A python package for detecting offensive usernames.
@@ -13,7 +14,9 @@ TRAINING_SET_FILENAME = 'data/generated/training_set.txt'
 
 def generate_training_set(n_samples: int, percent_offensive=0.5, percent_obfuscated=0.5, debug=False):
     """Create data which the KNN used to predict usernames is fit upon."""
-    pass
+
+    generate_usernames("./data/generated/training_usernames.txt", n_samples, percent_offensive, percent_obfuscated)
+    
 
 def predict_username_is_offensive(username: str) -> bool:
     """Given a username, predict whether it is offensive or inoffensive using a KNN fitted on generated usernames."""
