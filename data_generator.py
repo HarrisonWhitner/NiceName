b8694f9e301baf94382ef82e915cb614aa37060e
jhowe <jhowe@localhost.localdomain>
2020-12-11 14:33:34 -0500

    Added functionality to clean username data to ensure "good name"

--- data_generator.py
+++ data_generator.py
@@ -1,9 +1,9 @@
 import csv, random
 import utilities as utl
 
-adjectives = utl.file_to_array('./data/adjectives.txt')
-nouns = utl.file_to_array('./data/nouns.txt')
-names = utl.file_to_array('./data/names.txt')
+adjectives = utl.file_to_array('./data/adjectives_clean.txt')
+nouns = utl.file_to_array('./data/nouns_clean.txt')
+names = utl.file_to_array('./data/names_clean.txt')
 bad_words = utl.file_to_array('./data/bad_words_cmu.txt')
 bad_words_obfs = utl.file_to_array('./data/bad_words_google.txt')
 
@@ -29,16 +29,40 @@ def adjective_noun_generator(num_to_generate, obfuscation):
 
             outputFile.write(utl.print_name_data("G", username))
 
-def name_noun_generator(num_to_generate):
+def name_noun_generator(num_to_generate, obfuscation):
 
     with open("./data/good_name_noun.txt", 'w') as outputFile:
         for i in range(num_to_generate):
             ran_adj = random.choice(names)
             ran_noun = random.choice(nouns)
             username = ran_adj + ran_noun
+
+            if (obfuscation):
+                username = obfuscate(username)
+
             outputFile.write(utl.print_name_data("G", username))
+
+def clean_words():
+    for bad_word in bad_words:
+        if bad_word in nouns:
+            nouns.remove(bad_word)
+        if bad_word in adjectives:
+            adjectives.remove(bad_word)
+        if bad_word in names:
+            names.remove(bad_word)
+    
+    with open("./data/names_clean.txt", 'w') as names_file:
+        for name in names:
+            names_file.write(name + "\n")
+
+    with open("./data/nouns_clean.txt", 'w') as nouns_file:
+        for noun in nouns:
+            nouns_file.write(noun + "\n")
+
+    with open("./data/adjectives_clean.txt", 'w') as adjectives_file:
+        for adjective in adjectives:
+            adjectives_file.write(adjective + "\n")
 
 
 if __name__ == "__main__":
-    adjective_noun_generator(25, True)
-    name_noun_generator(25)
+    clean_words()
