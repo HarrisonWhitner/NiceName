452be03ebc80e4b6a70a38260ab83923f6c8a574
jhowe <jhowe@localhost.localdomain>
2020-12-11 15:19:37 -0500

    Completed username generation. Updated project organization and gitignore to reduce headaches.

--- utilities.py
+++ utilities.py
@@ -4,3 +4,27 @@ def file_to_array(string):
 
 def print_name_data(label, name):
     return label + "\t" + name + "\n"
+
+"""
+Code executed once for cleaning data. Archived for discussion lter
+"""
+# def clean_words():
+#     for bad_word in bad_words:
+#         if bad_word in nouns:
+#             nouns.remove(bad_word)
+#         if bad_word in adjectives:
+#             adjectives.remove(bad_word)
+#         if bad_word in names:
+#             names.remove(bad_word)
+    
+#     with open("./data/names_clean.txt", 'w') as names_file:
+#         for name in names:
+#             names_file.write(name + "\n")
+
+#     with open("./data/nouns_clean.txt", 'w') as nouns_file:
+#         for noun in nouns:
+#             nouns_file.write(noun + "\n")
+
+#     with open("./data/adjectives_clean.txt", 'w') as adjectives_file:
+#         for adjective in adjectives:
+#             adjectives_file.write(adjective + "\n")
