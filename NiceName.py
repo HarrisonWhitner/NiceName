8b6c30b23858b99e2455b68f056bae1b8a0d7786
jhowe <jhowe@localhost.localdomain>
2020-12-11 19:52:36 -0500

    Refactored ML components for modular appraoch to training / testing. Implemented testing of random samples.

--- NiceName.py
+++ NiceName.py
@@ -18,18 +18,19 @@ Creation Date: 11/28/20
 """ CONSTANTS """
 
 TRAINING_SET_FILENAME = 'data/generated/training_set.txt'
+TESTING_SET_FILENAME = 'data/generated/testing_set.txt'
 
 
 """ FUNCTIONS """
 
 
-def generate_training_set(n_samples: int, percent_offensive=0.5, percent_obfuscated=0.5, debug=False):
+def generate_data_set(n_samples: int, file_name, percent_offensive=0.5, percent_obfuscated=0.5, debug=False):
     """Create data which the KNN used to predict usernames is fit upon."""
 
     generate_usernames("./data/generated/training_usernames.txt", n_samples, percent_offensive, percent_obfuscated)
     username_array = util.file_to_array("./data/generated/training_usernames.txt")
 
-    with open(TRAINING_SET_FILENAME, 'w') as output_file:
+    with open(file_name, 'w') as output_file:
 
         for entry in username_array:
 
@@ -40,10 +41,19 @@ def generate_training_set(n_samples: int, percent_offensive=0.5, percent_obfusca
                 output_file.write(username + ' ' + str(jaro) + ' ' + str(lev) + ' ' + str(comparison) + " " + str(label) + '\n')
 
 
-def predict_username_is_offensive(username: str, n_neighbors=5, weights='uniform', debug=False) -> bool:
+def predict_username_is_offensive(username: str, model, debug=False) -> bool:
     """Given a username, predict whether it is offensive or inoffensive using a KNN fitted on generated usernames."""
     
-    knn_model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+    test_features = [generate_username_scores(username)]
+    test_target = model.predict(test_features)
+
+    #0 -> inoffensive, 1 -> offensive
+    print('The username', username, 'is predicted to be', 'offensive' if test_target[0] else 'inoffensive')
+    result = test_target[0]
+
+    return bool(test_target[0])
+
+def train_model(model):
     training_data = util.file_to_array(TRAINING_SET_FILENAME)
     features = []
     targets = []
@@ -53,21 +63,30 @@ def predict_username_is_offensive(username: str, n_neighbors=5, weights='uniform
         features.append([float(jaro), float(lev), float(comparison)])
         targets.append(int(label))
 
-    knn_model.fit(features, targets)
-    test_features = [generate_username_scores(username)]
-    test_target = knn_model.predict(test_features)
+    model.fit(features, targets)
 
-    #0 -> inoffensive, 1 -> offensive
-    print('The username', username, 'is predicted to be', 'offensive' if test_target[0] else 'inoffensive')
-    result = test_target[0]
+def calculate_accuracy(model):
 
-    return bool(test_target[0])
+    training_data = util.file_to_array(TESTING_SET_FILENAME)
+    features = []
+    targets = []
+
+    for row in training_data:
+        curName, jaro, lev, comparison, label = row.split()
+        features.append([float(jaro), float(lev), float(comparison)])
+        targets.append(int(label))
+
+    return model.score(features, targets)
 
 
 def main():
-    generate_training_set(100, .5, .5)
+    generate_data_set(100, TRAINING_SET_FILENAME, .5, .5)
+    generate_data_set(100, TESTING_SET_FILENAME, .5, .5)
+    knn_model = KNeighborsClassifier(5)
+    train_model(knn_model)
+    print(calculate_accuracy(knn_model))
     while True:
-        predict_username_is_offensive(input('Enter a username to score: '))
+        predict_username_is_offensive(input('Enter a username to score: '), knn_model)
 
 if __name__ == "__main__":
     main()
